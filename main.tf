5649a1933724bd56231a4b00402fcc9789757478
drhaynes <drhmbp@gmail.com>
2019-09-26 10:12:49 +0100

    Add VPC creation

--- main.tf
+++ main.tf
@@ -5,3 +5,10 @@ provider "aws" {
     version = "~> 2.29"
     region = "eu-west-1"
 }
+
+resource "aws_vpc" "main" {
+    cidr_block = "10.1.0.0/16"
+    tags = {
+        Name = "Terraform Testing VPC"
+    }
+}
